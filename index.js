7636d40511efd91b3c0faada0296b886dac499a9
heyjan <47437411+heyjan@users.noreply.github.com>
2023-09-24 02:22:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, MessageEmbed } = require('discord.js');
 const { setInterval } = require('timers');
 
 const app = express();
@@ -24,13 +24,26 @@ client.once('ready', () => {
     setInterval(async () => {
         if (messageQueue.length > 0) {
             const data = messageQueue.shift();
-            console.log(`Sending to Discord: ${data}`);
+            const transaction = data.transfer;
 
+            // Create the embed
+            const embed = new MessageEmbed()
+                .setTitle("New Token Transfer!")
+                .setColor(0x3498db) // A nice blue color; you can change this
+                .addField('Token Symbol', transaction.tokenSymbol, true)
+                .addField('Chain', transaction.chain, true)
+                .addField('\u200B', '\u200B')                       // Empty field to create a little space
+                .addField('Amount', `${transaction.unitValue}`, true)
+                .addField('Amount in USD', `$${parseFloat(transaction.historicalUSD).toFixed(2)}`, true)
+                .addField('tx', `[View on Etherscan](https://etherscan.io/tx/${transaction.transactionHash})`, true)
+                .addField('tx', `[View on Arkham](https://platform.arkhamintelligence.com/explorer/tx/${transaction.transactionHash})`, true);
+                .addField('time of transaction', transaction.blockTimestamp, false)
+                
+        
+            console.log(`Sending to Discord: ${JSON.stringify(data)}`);
             const channel = await client.channels.fetch(CHANNEL_ID);
-            const content = `Transaction Alert: ${JSON.stringify(data)}`;
-
             try {
-                await channel.send(content);
+                await channel.send({ embeds: [embed] });
             } catch (e) {
                 console.error(`Error sending message: ${e}`);
             }
